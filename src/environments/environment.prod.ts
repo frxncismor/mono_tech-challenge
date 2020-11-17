8f92720686122104a2cf963f6f53857ee1095b82
frxncismor <frxncismor@gmail.com>
2020-11-17 16:40:51 -0600

    [FIX] ev for vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: '${process.env.API_URL}',
+  API_KEY: '${process.env.API_KEY}',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: false,
-  API_URL: '${secrets.API_URL}',
-  API_KEY: '${secrets.API_KEY}',
 };
