910c60c272e694fccecf83e337331012ebc91dc1
frxncismor <frxncismor@gmail.com>
2020-11-18 20:39:03 -0600

    [FIX] Hidding environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'https://api.stripe.com',
-  API_KEY:
-    'sk_test_51HmRonJILryrAveDMWp6i4VK3WA8qx3rvUS70Hrx7qPXgt6F3E7dYNkg5NsYCl6VQN96K456Y8q54RtIe3VdUZCo00r1kYPfu8',
+  API_URL: `${process.env.API_URL}`,
+  API_KEY: `${process.env.API_URL}`,
 };
